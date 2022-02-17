c174deab8c03b6650ad6881659912577bcf22f16
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-02-17 11:22:43 +0100

    Moved morgan to log all except file fetches.

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ app.engine(
         extname: ".hbs", // Filename extension
     })
 );
-// Custom token for printing in the console whevener we recieve a request.
-// Place this before any app.set app.use.
-new CustomMorganToken(morgan, app).enable();
+
 // Setting template engine
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views")); // Tells express where the views folder is located.
@@ -30,10 +28,15 @@ app.use(express.urlencoded({ extended: true })); // Needed for forms to work.
 app.use(express.json()); // Tells the server to expect request info to be in JSON format.
 // Static files
 app.use(express.static("./src/public")); // Public folder. Css and  JS access.
+// Custom token for printing in the console whevener we recieve a request.
+// We placed it here specifically to avoid morgan printing getting js, css, favicon etc, but
+// to include routers and routes.
+new CustomMorganToken(morgan, app).enable();
+// Routers
 app.use("/todos", todoRouter); // Tells the server to use our todoRouter on "/todo" urls.
 // ####################### ROUTES #######################
 app.get("/", (req, res) => {
-    res.render("home");
+    res.status(200).render("home");
 });
 
 // After all routes have been declared we implement a / route to
